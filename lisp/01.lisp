c42058dbe8860689fd4bc991c5fd5dae5a985d57
Chris Sarbora <tophyr@gmail.com>
2024-12-10 13:18:51 -0600

    01 lisp improvement: pre-load data into a variable

--- lisp/01.lisp
+++ lisp/01.lisp
@@ -1,19 +1,29 @@
 (print
     (if (= 0 (length (uiop:command-line-arguments)))
         (list "Usage: script <dat-file>")
-        (apply '+
-            (mapcar
-                (lambda (left right) (abs (- left right)))
-                (sort
+        (let
+            (
+                (numpairs
                     (mapcar
-                        (lambda (line) (parse-integer (first (uiop:split-string line :separator " "))))
-                        (uiop:read-file-lines (first (uiop:command-line-arguments))))
-                    '<)
-                (sort
-                    (mapcar
-                        (lambda (line) (parse-integer (first (last (uiop:split-string line :separator " ")))))
-                        (uiop:read-file-lines (first (uiop:command-line-arguments))))
-                    '<)
+                        (lambda (line)
+                            (mapcar
+                                (lambda (str) (parse-integer str))
+                                (remove-if
+                                    (lambda (str) (= 0 (length str)))
+                                    (uiop:split-string line :separator " ")
+                                )
+                            )
+                        )
+                        (uiop:read-file-lines (first (uiop:command-line-arguments)))
+                    )
+                )
+            )
+            (apply '+
+                (mapcar
+                    (lambda (left right) (abs (- left right)))
+                    (sort (mapcar (lambda (numpair) (first numpair)) numpairs) '<)
+                    (sort (mapcar (lambda (numpair) (second numpair)) numpairs) '<)
+                )
             )
         )
     )
