ab21538d2fa2c49bf80ea93faa25f0dde8cf112c
Chris Sarbora <tophyr@gmail.com>
2025-01-06 12:05:39 -0600

    09 10 rust

---
+++ rust/10/src/main.rs
@@ -0,0 +1,115 @@
+use std::cmp::Ordering;
+use std::env;
+use std::fs::read_to_string;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Reprint {
+    pages: Vec<i32>
+}
+#[derive(Debug, PartialEq, Eq)]
+struct ParseReprintError;
+
+impl Reprint {
+    fn middle_page(&self) -> Option<&i32> {
+        self.pages.get(self.pages.len() / 2)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Rule {
+    before: i32,
+    after: i32
+}
+#[derive(Debug, PartialEq, Eq)]
+struct ParseRuleError;
+
+impl Rule {
+    fn check(&self, reprint: &Reprint) -> bool {
+        reprint.pages.is_sorted_by(|l, r| self.ord(l, r) != Ordering::Greater)
+    }
+
+    fn fix(&self, reprint: &mut Reprint) {
+        reprint.pages.sort_by(|l, r| self.ord(l, r));
+    }
+
+    fn ord(&self, l: &i32, r: &i32) -> Ordering {
+        if *l == self.before && *r == self.after {
+            return Ordering::Less;
+        } else if *l == self.after && *r == self.before {
+            return Ordering::Greater;
+        } else {
+            return Ordering::Equal;
+        }
+    }
+}
+
+impl FromStr for Reprint {
+    type Err = ParseReprintError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Reprint {
+            pages: s.split(',')
+                .map(|p| p.parse::<i32>())
+                .collect::<Result<Vec<_>, _>>()
+                .map_err(|_| Self::Err{})?
+        })
+    }
+}
+
+impl FromStr for Rule {
+    type Err = ParseRuleError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.split_once('|')
+            .and_then(|(b, a)| {
+                Some(Rule {
+                    before: b.parse::<i32>().ok()?,
+                    after: a.parse::<i32>().ok()?
+                })
+            })
+            .ok_or(Self::Err{})
+    }
+}
+
+fn read_data() -> (Vec<Rule>, Vec<Reprint>) {
+    if env::args().count() < 2 {
+        panic!("Usage: {} <dat-file>", env::args().nth(0).unwrap());
+    }
+
+    let lines = read_to_string(env::args().nth(1).unwrap()).unwrap()
+        .lines()
+        .map(|e| e.into())
+        .collect::<Vec<String>>();
+    let mut split = lines.split(|l| l.is_empty());
+    let rules = split.next().unwrap_or(&[]).iter()
+        .filter_map(|str| FromStr::from_str(str).ok())
+        .collect::<Vec<Rule>>();
+    let reprints = split.next().unwrap_or(&[]).iter()
+        .filter_map(|str| FromStr::from_str(str).ok())
+        .collect::<Vec<Reprint>>();
+
+    return (rules, reprints);
+}
+
+fn main() {
+    let (rules, reprints) = read_data();
+    
+    let middle_sum : i32 = reprints.iter()
+        .filter(|e| !rules.iter().all(|r| r.check(e)))
+        .map(|e| {
+            let mut fixed = e.clone();
+            loop {
+                if rules.iter().all(|r| r.check(&fixed)) {
+                    return fixed;
+                }
+                for rule in &rules {
+                    rule.fix(&mut fixed);
+                }
+            }
+        })
+        .filter_map(|e| e.middle_page().map(|v| *v))
+        .sum();
+
+    println!("{middle_sum}");
+}
